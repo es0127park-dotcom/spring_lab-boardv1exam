8d9088e5cfe3f9115227933739eb582b5e99973a
parkcoding <es0127.park@gmail.com>
2026-02-10 17:16:28 +0900

    실습 시작

---
+++ src/main/java/com/example/boardv1exam/Boardv1examApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardv1exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boardv1examApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boardv1examApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boardv1exam/Boardv1examApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardv1exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boardv1examApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
