07184d91416e1cd48cfd58625dee6ba0fb5a31ef
parkcoding <es0127.park@gmail.com>
2026-02-10 17:31:41 +0900

    초기 세팅 및 board 엔티티 생성(part1-ch01,ch02)

---
+++ src/main/java/com/example/boardv1exam/board/Board.java
@@ -0,0 +1,28 @@
+package com.example.boardv1exam.board;
+
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "board_tb")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String content;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+}
